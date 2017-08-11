ad257b5ee38be2330bfb906794f239bd89047eba
David Dong <dongm2ez@163.com>
2017-08-11 10:32:40 +0800

    user数据白名单

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'role_id', 'name', 'email', 'avatar', 'password', 'register_source', 'github_id', 'github_url', 'wechat_openid', 'wechat_unionid'
+        'name', 'email', 'avatar', 'password', 'register_source', 'github_id', 'github_url', 'wechat_openid', 'wechat_unionid'
     ];
 
     /**
